2e8a8480b5e89ac26fbed9fe490cb6589908b539
Danial Pourmehdi <danialpourmehdi@gmail.com>
2026-05-04 19:24:47 +0330

    updateing

--- kiosk/settings.py
+++ kiosk/settings.py
@@ -76,18 +76,24 @@ WSGI_APPLICATION = 'kiosk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER':'postgres',
-        'PASSWORD':'P@ssw0rd',
-        'HOST':'localhost', 
-        'PORT':'5432',
-        
+        'ENGINE': 'django.db.backends.sqlite3',  # Note the closing quote and comma
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydb',
+#         'USER':'postgres',
+#         'PASSWORD':'P@ssw0rd',
+#         'HOST':'localhost', 
+#         'PORT':'5432',
+        
+#     }
+# }
 
 
 # Password validation
